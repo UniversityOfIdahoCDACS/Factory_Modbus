16bfdd2036614aed29543bd02600a8f324671851
Nick Krenowicz <kren5141@vandals.uidaho.edu>
2022-04-27 00:21:19 -0700

    webapp start, logging, add job

--- pyController/test_webapp.py
+++ pyController/test_webapp.py
@@ -1,22 +1,35 @@
+""" Test file for webadmin.py """
 
 from time import sleep
+import logging
 import webapp.webadmin as webadmin
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG) # sets default logging level for this module
+
+# Create formatter
+#formatter = logging.Formatter('[%(asctime)s] [%(levelname)-5s] [%(name)s] [%(threadName)s] - %(message)s')
+formatter = logging.Formatter('[%(asctime)s] [%(levelname)-5s] [%(name)s] - %(message)s')
+
+# Logger: create console handle
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)     # set logging level for console
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 def main():
     """ Main program """
     print("Starting Webapp")
-    
+
     webadmin.start_webapp()
     print("Webapp continuted")
 
     while True:
-        print("tick")
+        # print("tick")
         try:
             sleep(5)
         except KeyboardInterrupt:
             print("main exiting from keyboard interrupt")
-            webadmin.stop_webapp()
             exit()
 
 
--- pyController/webapp/webadmin.py
+++ pyController/webapp/webadmin.py
@@ -7,19 +7,14 @@ from flask import Flask, render_template, redirect, send_file, url_for, request,
 import waitress  # Production webserver
 
 # Append parent directory to import path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) # Directory of this script
+sys.path.insert(0, os.path.dirname(SCRIPT_DIR))
 
 from job_data import JobData
 
-# starting value for 
-job_id = 500
-order_id = 8000
+app = Flask('FactoryAdmin', static_url_path="/static", static_folder=SCRIPT_DIR + '/static', template_folder=SCRIPT_DIR + '/templates')
 
-
-script_dir = os.path.dirname(os.path.realpath(__file__))    # Directory of this script
-app = Flask('FactoryAdmin', static_url_path="/static", static_folder=script_dir + '/static')
-
-class webapp_storage():
+class WebappStorage():
     """ This class holds function callbacks, objects and attributes.
     These may be needed by both this webapp and outside objects like pyController
     Using this as flask apps can't easily be packaged into a class
@@ -29,12 +24,26 @@ class webapp_storage():
         self._factory_command_cb = None
         self.orchastrator = None
         self.server = None
+        self.job_id = 0
+        self.order_id = 0
+
+    def get_a_job_id(self):
+        """ Return a job id then increment """
+        val = self.job_id
+        self.job_id += 1
+        return val
+
+    def get_an_order_id(self):
+        """ Return a order id then increment """
+        val = self.order_id
+        self.order_id += 1
+        return val
 
     def set_orchastrator(self, obj):
         """ Called to set the orchastrator object """
         self.orchastrator = obj
 
-    def add_order_cb(self, job_data):
+    def add_job_cb(self, job_data):
         """ Adds job to orchastrator """
         if self.orchastrator:
             self.orchastrator.add_job_callback(job_data)
@@ -45,44 +54,32 @@ class webapp_storage():
             self.orchastrator.factory_command_callback()
 
 
-
-
 def worker():
     """ Worker thread to run webapp """
     # Look at https://stackoverflow.com/questions/15562446/how-to-stop-flask-application-without-using-ctrl-c
     global app
     app.run(host="0.0.0.0", port=10002, threaded=True) # Development server
-    #waitress.serve(app, host='0.0.0.0', port=10002)     # Production server
-    print("HI do you see me?")  
+    # waitress.serve(app, host='0.0.0.0', port=10002)     # Production server
+    print("HI do you see me?")
 
 def start_webapp():
     """ Start the webserver """
     worker_thread.start()
 
-def start_webapp2():
-    """ Start the webserver """
-    callbacks.server = waitress.create_server(app, host='0.0.0.0', port=10002)
-
-
-def stop_webapp():
-    """ Stop the webserver """
-    print("Stop webapp called.")
-    callbacks.server.close()
-
-
-@app.route("/")
-@app.route("/index")
-def serve_root():
-    return render_template("index.html")
 
 # https://github.com/ColorlibHQ/AdminLTE/releases
 # https://github.com/tbotnz/flask-AdminLTE
 # https://adminlte.io/themes/v3/pages/UI/timeline.html#
 # http://localhost:10001/starter#
 
+#############################
+## App calls
+############################
+
 @app.route("/myfunction_1")
 # https://syntaxfix.com/question/1571/flask-calling-python-function-on-button-onclick-event
 def myfunction_1():
+    """ Test Function """
     print("hello there from myfunction_1")
     return "Nothing"
 
@@ -97,7 +94,8 @@ def create_order():
         # checkbox does not show when faulst
         # check if not exist and consider it false
         try:
-            job = JobData(job_id=job_id, order_id=order_id, color=req['order_color'], cook_time=int(req['order_CookTime']), sliced=True)
+            job = JobData(job_id=webapp_storage.get_a_job_id(), order_id=webapp_storage.get_an_order_id(),
+                          color=req['order_color'], cook_time=int(req['order_CookTime']), sliced=True)
         except TypeError:
             print("Browser sent bad info")
             return redirect("/")
@@ -107,23 +105,31 @@ def create_order():
             return redirect("/")
         else:
             print("Sending job order to Orchastrator")
-            #callbacks.add_order_cb(job)
-            callbacks.orchastrator.add_job_callback(job)
+            webapp_storage.add_job_cb(job)
 
     return redirect("/")
+
+#############################
+## App routing
+############################
+
+@app.route("/")
+@app.route("/index")
+def serve_root():
+    """ Serve root index """
+    return render_template("index.html")
+
 
 @app.route("/favicon.ico")
 # https://flask.palletsprojects.com/en/1.1.x/patterns/favicon/
 def favicon():
     """ Serve favicon """
     print("favicon here")
-    return send_file('favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_file(SCRIPT_DIR + '/favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
-callbacks = webapp_storage()
-worker_thread = threading.Thread(target=worker)
+webapp_storage = WebappStorage()
+worker_thread = threading.Thread(target=worker, daemon=True)
 
 if __name__ == "__main__":
-    #worker()
-    start_webapp2()
-    print("Heloo")
+    start_webapp()
